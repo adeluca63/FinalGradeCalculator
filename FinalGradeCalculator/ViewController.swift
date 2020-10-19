f30eb87371188a0863867a62cef2ea3bc21df825
adeluca63 <aidanjohndeluca@gmail.com>
2020-10-19 09:35:00 -0400

    Basic Outline

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var currentGrade: UITextField!
     @IBOutlet weak var lastTest: UITextField!
+    @IBOutlet weak var desiredGrade: UITextField!
     @IBOutlet weak var myLabel: UILabel!
     
     override func viewDidLoad()
@@ -24,6 +25,25 @@ class ViewController: UIViewController
     @IBAction func calculateGrade(_ sender: UIButton)
     
     {
+    
+    let num = currentGrade.text ?? "0"
+    let num1 = Double(num) ?? 0
+    
+    let Num = lastTest.text ?? "0"
+    let num2 = Double(Num) ?? 0
+    
+    let NuM = desiredGrade.text ?? "0"
+    let num3 = Double(NuM) ?? 0
+    
+    //let result = num3 - num1 + num2 * 0.8 / 0.2
+        
+    // T = Grade * 0.8 + Final * 0.2
+    // Figure out problem above (Algebraic Equation)
+    
+    myLabel.text = String(result)
+    currentGrade.text = ""
+    lastTest.text = ""
+    desiredGrade.text = ""
     
     }
     
